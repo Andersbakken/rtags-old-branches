0d833c508e722fec5d98ac37f38416a766d861aa
Anders Bakken <agbakken@gmail.com>
2012-03-05 14:59:19 -0800

    Fix stat to report pch files as dirty

--- makelib/makelib.cpp
+++ makelib/makelib.cpp
@@ -10,23 +10,34 @@ typedef int (*RealStat)(int, const char*, struct stat64*);
 RealStat realStat = 0;
 int __xstat64 (int ver, const char *filename, struct stat64 *stat_buf)
 {
+    static const bool debug = getenv("DEBUG_STAT");
+    FILE *f = 0;
+    if (debug)
+        f = fopen("/tmp/log", "a");
     if (!realStat) {
         realStat = reinterpret_cast<RealStat>(dlsym(RTLD_NEXT, "__xstat64"));
     }
     int ret = realStat(ver, filename, stat_buf);
     if (!ret && S_ISREG(stat_buf->st_mode)) {
-        const int len = strlen(filename);
         bool changed = false;
+        const int len = strlen(filename);
         if (len >= 2 && !strncmp(filename + len - 2, ".o", 2)) {
             stat_buf->st_mtime = 1;
             changed = true;
         } else if (len >= 3 && !strncmp(filename + len - 3, ".lo", 3)) {
             stat_buf->st_mtime = 1;
             changed = true;
+        } else if (len >= 3 && !strncmp(filename + len - 3, "c++", 3)) {
+            stat_buf->st_mtime = 1;
+            changed = true;
+        } else if (len >= 1 && !strncmp(filename + len - 1, "c", 1)) {
+            stat_buf->st_mtime = 1;
+            changed = true;
         }
-        static bool debug = getenv("DEBUG_STAT");
-        if (debug)
-            fprintf(stderr, "stated [%s]%s\n", filename, changed ? " changed" : "");
+        if (f)
+            fprintf(f, "stated [%s]%s\n", filename, changed ? " changed" : "");
     }
+    if (f)
+        fclose(f);
     return ret;
 }
