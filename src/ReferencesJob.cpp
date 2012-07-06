96467fa510bfa7cb698b8d0fa2799894afe513dc
Anders Bakken <agbakken@gmail.com>
2012-07-06 16:36:23 -0700

    This is actually in the wrong location so I'll revert it but just in case it turns out to be something we want I'll leave the revert in :-)

--- src/ReferencesJob.cpp
+++ src/ReferencesJob.cpp
@@ -15,6 +15,30 @@ ReferencesJob::ReferencesJob(int i, const ByteArray &sym, unsigned fl)
 {
 }
 
+struct LocationAndDefinitionNode
+{
+    LocationAndDefinitionNode(const Location &loc, bool def)
+        : location(loc), isDefinition(def)
+    {}
+    LocationAndDefinitionNode() {}
+    Location location;
+    bool isDefinition;
+
+    bool operator<(const LocationAndDefinitionNode &other) const
+    {
+        if (isDefinition != other.isDefinition)
+            return isDefinition;
+        return location < other.location;
+    }
+    bool operator>(const LocationAndDefinitionNode &other) const
+    {
+        if (isDefinition != other.isDefinition)
+            return !isDefinition;
+        return location > other.location;
+    }
+
+};
+
 void ReferencesJob::execute()
 {
     if (!symbolName.isEmpty()) {
@@ -27,7 +51,7 @@ void ReferencesJob::execute()
     const bool excludeDefsAndDecls = !(flags & QueryMessage::IncludeDeclarationsAndDefinitions);
     ScopedDB db = Server::instance()->db(Server::Symbol, ReadWriteLock::Read);
     const unsigned keyFlags = QueryMessage::keyFlags(flags);
-    Set<Location> refs;
+    Map<Location, bool> refs;
     Set<Location> filtered;
     for (Set<Location>::const_iterator it = locations.begin(); it != locations.end(); ++it) {
         if (isAborted())
@@ -37,53 +61,56 @@ void ReferencesJob::execute()
         Location realLoc;
         CursorInfo cursorInfo = RTags::findCursorInfo(db, location, &realLoc);
         if (RTags::isReference(cursorInfo.kind)) {
-            if (excludeDefsAndDecls) {
-                filtered.insert(cursorInfo.target);
-            } else {
-                refs.insert(cursorInfo.target);
-            }
+            const Location target = cursorInfo.target;
             cursorInfo = RTags::findCursorInfo(db, cursorInfo.target);
+            if (excludeDefsAndDecls) {
+                filtered.insert(target);
+            } else {
+                refs[target] = cursorInfo.isDefinition;
+            }
         } else {
             if (excludeDefsAndDecls) {
                 filtered.insert(realLoc);
             } else {
-                refs.insert(realLoc);
+                refs[realLoc] = cursorInfo.isDefinition;
             }
         }
 
         if (cursorInfo.symbolLength) {
-            if (cursorInfo.target.isValid()) {
-                if (excludeDefsAndDecls) {
-                    filtered.insert(cursorInfo.target);
-                } else {
-                    refs.insert(cursorInfo.target);
-                }
+            if (cursorInfo.target.isValid() && excludeDefsAndDecls) {
+                filtered.insert(cursorInfo.target);
             }
             for (Set<Location>::const_iterator it = cursorInfo.references.begin(); it != cursorInfo.references.end(); ++it) {
                 const Location &l = *it;
                 if (!excludeDefsAndDecls || !filtered.contains(l)) {
-                    refs.insert(l);
+                    refs[l] = false; // none of these should be definitions right?
                 }
             }
             if (cursorInfo.target.isValid() && cursorInfo.kind != CXCursor_VarDecl) {
+                const Location l = cursorInfo.target;
                 cursorInfo = RTags::findCursorInfo(db, cursorInfo.target);
+                refs[l] = cursorInfo.isDefinition;
                 for (Set<Location>::const_iterator it = cursorInfo.references.begin(); it != cursorInfo.references.end(); ++it) {
                     const Location &l = *it;
                     if (!excludeDefsAndDecls || !filtered.contains(l)) {
-                        refs.insert(l);
+                        refs[l] = false;
                     }
                 }
             }
         }
     }
-    List<Location> sorted = refs.toList();
+    List<LocationAndDefinitionNode> sorted(refs.size());
+    int i = 0;
+    for (Map<Location, bool>::const_iterator it = refs.begin(); it != refs.end(); ++it) {
+        sorted[i++] = LocationAndDefinitionNode(it->first, it->second);
+    }
+
     if (flags & QueryMessage::ReverseSort) {
-        std::sort(sorted.begin(), sorted.end(), std::greater<Location>());
+        std::sort(sorted.begin(), sorted.end(), std::greater<LocationAndDefinitionNode>());
     } else {
         std::sort(sorted.begin(), sorted.end());
     }
-    for (List<Location>::const_iterator it = sorted.begin(); it != sorted.end(); ++it) {
-        const Location &l = *it;
-        write(l.key(keyFlags));
+    for (List<LocationAndDefinitionNode>::const_iterator it = sorted.begin(); it != sorted.end(); ++it) {
+        write(it->location.key(keyFlags));
     }
 }
